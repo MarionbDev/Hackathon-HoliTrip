01fe0ca522f16eb441b6334a09cba5764573403d
JeanBat0202 <jeanbaptiste.blond@gmail.com>
2023-05-12 10:17:14 +0200

    creation page Brazil et Australie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
+import Canada from "@pages/Canada";
+import Australia from "@pages/Australia";
+import Brazil from "@pages/Brazil";
 import "./App.css";
 import "./components/loading.css";
+import Header from "@components/Header";
 import RadioRoad from "./components/RadioRoad";
+
 import {
   Toronto,
   Ottawa,
@@ -30,35 +35,38 @@ import {
 
 function App() {
   return (
-    <main>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/canada" element={<Canada />} /> */}
-          <Route path="/canada/toronto" element={<Toronto />} />
-          <Route path="/canada/ottawa" element={<Ottawa />} />
-          <Route path="/canada/montreal" element={<Montreal />} />
-          <Route path="/canada/quebec" element={<Quebec />} />
-          <Route path="/canada/thunder_bay" element={<ThunderBay />} />
-          <Route path="/canada/winnipeg" element={<Winnipeg />} />
-          {/* <Route path="/australia" element={<Australia />} /> */}
-          <Route path="/australia/brisbane" element={<Brisbane />} />
-          <Route path="/australia/sydney" element={<Sydney />} />
-          <Route path="/australia/canberra" element={<Canberra />} />
-          <Route path="/australia/melbourne" element={<Melbourne />} />
-          <Route path="/australia/auburn" element={<Auburn />} />
-          <Route path="/australia/adelaide" element={<Adelaide />} />
-          {/* <Route path="/brazil" element={<Peru />} /> */}
-          <Route path="/brazil/rio" element={<Rio />} />
-          <Route path="/brazil/sao_paulo" element={<SaoPaulo />} />
-          <Route path="/brazil/curitiba" element={<Curitiba />} />
-          <Route path="/brazil/porto_alegre" element={<PortoAlegre />} />
-          <Route path="/brazil/petropolis" element={<Petropolis />} />
-          <Route path="/brazil/juiz_de_fora" element={<JuizDeFora />} />
-          <Route path="/radio" element={<RadioRoad />} />
-        </Routes>
-      </div>
-    </main>
+    <>
+      <Header />
+      <main>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/canada" element={<Canada />} />
+            <Route path="/canada/toronto" element={<Toronto />} />
+            <Route path="/canada/ottawa" element={<Ottawa />} />
+            <Route path="/canada/montreal" element={<Montreal />} />
+            <Route path="/canada/quebec" element={<Quebec />} />
+            <Route path="/canada/thunder_bay" element={<ThunderBay />} />
+            <Route path="/canada/winnipeg" element={<Winnipeg />} />
+            <Route path="/australia" element={<Australia />} />
+            <Route path="/australia/brisbane" element={<Brisbane />} />
+            <Route path="/australia/sydney" element={<Sydney />} />
+            <Route path="/australia/canberra" element={<Canberra />} />
+            <Route path="/australia/melbourne" element={<Melbourne />} />
+            <Route path="/australia/auburn" element={<Auburn />} />
+            <Route path="/australia/adelaide" element={<Adelaide />} />
+            <Route path="/brazil" element={<Brazil />} />
+            <Route path="/brazil/rio" element={<Rio />} />
+            <Route path="/brazil/sao_paulo" element={<SaoPaulo />} />
+            <Route path="/brazil/curitiba" element={<Curitiba />} />
+            <Route path="/brazil/porto_alegre" element={<PortoAlegre />} />
+            <Route path="/brazil/petropolis" element={<Petropolis />} />
+            <Route path="/brazil/juiz_de_fora" element={<JuizDeFora />} />
+            <Route path="/radio" element={<RadioRoad />} />
+          </Routes>
+        </div>
+      </main>
+    </>
   );
 }
 export default App;
