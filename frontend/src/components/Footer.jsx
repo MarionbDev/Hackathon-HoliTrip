8fc45fc0ecb38f48455303e3799dad01066e2a0c
Benjamin Laroche <benjamin.laroche28@gmail.com>
2023-05-12 12:00:57 +0200

    footer finished with no link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,29 @@
+// import { Router, Link } from "react-router-dom";
+
 function Footer() {
   return (
     <footer>
       <div className="footer">
+        {/* <Link to="/"> */}
+        <div className="logo-site">
+          <img
+            className="logo-footer"
+            src="/src/assets/logohackathon.png"
+            alt="Logo du site"
+          />
+        </div>
+        {/* </Link> */}
         <p> ©℗®™ Holitrip </p>
         <div className="logo-container">
           <img
             className="logo"
             src="/src/assets/logo_Facebook.svg"
-            alt="Logo 1"
+            alt="Logo facebook"
           />
           <img
             className="logo"
             src="/src/assets/logo_Instagram.svg"
-            alt="Logo 2"
+            alt="Logo instagram"
           />
         </div>
       </div>
