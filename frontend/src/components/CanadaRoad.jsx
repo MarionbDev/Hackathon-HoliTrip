93c594d8b76cf19f7e3bb8420db6125857fa54bd
MarionbDev <marionbaston84@gmail.com>
2023-05-12 10:44:55 +0200

    composant pays road

--- frontend/src/components/CanadaRoad.jsx
+++ frontend/src/components/CanadaRoad.jsx
@@ -1,38 +1,21 @@
 import City from "./VisitCity";
+import City2 from "./VisitCity2";
 
 const Toronto = () => {
   return (
     <div>
-      <City
-        cityId="N4816874223"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="Q4284139"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="W4942687"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
+      <City cityId="N4816874223" />
+      <City2 cityId="Q4284139" />
+      <City cityId="W4942687" />
     </div>
   );
 };
 const Ottawa = () => {
   return (
     <div>
-      <City
-        cityId="W68588608"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="N4568910828"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="W223069822"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
+      <City cityId="W68588608" />
+      <City2 cityId="N4568910828" />
+      <City cityId="W223069822" />
     </div>
   );
 };
@@ -40,18 +23,9 @@ const Ottawa = () => {
 const Quebec = () => {
   return (
     <div>
-      <City
-        cityId="Q3324394"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="Q2114279"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="R196002"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
+      <City cityId="Q3324394" />
+      <City2 cityId="Q2114279" />
+      <City cityId="R196002" />
     </div>
   );
 };
@@ -59,18 +33,9 @@ const Quebec = () => {
 const Montreal = () => {
   return (
     <div>
-      <City
-        cityId="W20168831"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="N1330276952"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="Q16508755"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
+      <City cityId="W20168831" />
+      <City2 cityId="N1330276952" />
+      <City cityId="Q16508755" />
     </div>
   );
 };
@@ -78,36 +43,18 @@ const Montreal = () => {
 const ThunderBay = () => {
   return (
     <div>
-      <City
-        cityId="N5117185631"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="Q23653064"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="Q877280"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
+      <City cityId="N5117185631" />
+      <City2 cityId="Q23653064" />
+      <City cityId="Q877280" />
     </div>
   );
 };
 const Winnipeg = () => {
   return (
     <div>
-      <City
-        cityId="Q6750086"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="N343302576"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
-      <City
-        cityId="W25273914"
-        apiKey="5ae2e3f221c38a28845f05b6f386be0cdad7935fb4ef722b54e158e4"
-      />
+      <City cityId="Q6750086" />
+      <City2 cityId="N343302576" />
+      <City cityId="W25273914" />
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,18 +2,21 @@ import { Link } from "react-router-dom";
 import Carrousel from "@components/Carousel";
 import Presentation from "@components/Presentation";
 import ProposalList from "@components/ProposalList";
+import RadioLogo from "../assets/radio/radioLogo.png";
 // import MoodBoard from "@components/MoodBoard";
 
 export default function Home() {
   return (
     <header className="App-header">
+      <Link to="/radio">
+        <button type="button">
+          <img className="md:w-20 md:h-20" src={RadioLogo} alt={RadioLogo} />
+        </button>
+      </Link>
       <Carrousel />
       <Presentation />
       <ProposalList />
       {/* <MoodBoard /> */}
-      <Link to="/radio">
-        <button type="button">RADIO</button>
-      </Link>
     </header>
   );
 }
